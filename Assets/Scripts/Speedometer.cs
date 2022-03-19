1bcbbce541cf4564a5efecb10985684a0d63f738
Matt Friendly <dj@mattfriendly.com>
2022-03-18 22:26:00 -0700

    fix: Added friendly commentar

--- Assets/Scripts/Speedometer.cs
+++ Assets/Scripts/Speedometer.cs
@@ -15,7 +15,7 @@ public class Speedometer : MonoBehaviour
 
     private void Update()
     {
-        speed = target.velocity.magnitude * 3.6f * 1.09f;
+        speed = target.velocity.magnitude * 3.6f * 1.09f; // tweak speed here
 
         if (speedLabel != null)
             speedLabel.text = ((int)speed) + "";
