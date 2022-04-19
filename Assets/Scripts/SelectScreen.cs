9f50acefe9448b985a9c5a543d6ad6c87bd091c3
Dustin "Foxx" Musser <dustsoft@gmail.com>
2022-04-19 12:33:26 -0600

    Update 4-19
    Bug Fix

--- Assets/Scripts/SelectScreen.cs
+++ Assets/Scripts/SelectScreen.cs
@@ -7,11 +7,25 @@ public class SelectScreen : MonoBehaviour
 {
     public void LoadCityNights()
     {
+        LapTimeManager.MinuteCount = 0;
+        LapTimeManager.SecondCount = 0;
+        LapTimeManager.MilliCount = 0;
+        LapTimeManager.MilliCountX = 0;
+
+        LapTimeManager.RawTime = 0;
+
         SceneManager.LoadScene("City Nights (Track 01) Scene");
     }
 
     public void LoadStellarRoads()
     {
+        LapTimeManager.MinuteCount = 0;
+        LapTimeManager.SecondCount = 0;
+        LapTimeManager.MilliCount = 0;
+        LapTimeManager.MilliCountX = 0;
+
+        LapTimeManager.RawTime = 0;
+
         SceneManager.LoadScene("Stellar Roads (Track 02) Scene");
     }
 }
